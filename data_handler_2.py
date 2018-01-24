e00b15d885b0d0e6700feeb60d2a88b52baabc0a
Tamás Varjas <tamas.varjas00@gmail.com>
2018-01-24 21:13:02 +0100

    comment section bug fixed and distinction between question-comments and answer-comments solved

--- data_handler_2.py
+++ data_handler_2.py
@@ -4,13 +4,13 @@ import database_common
 
 def hash_password(plain_text_password):
     hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
-    
+
     return hashed_bytes.decode('utf-8')
 
 
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
-    
+
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
 
 
@@ -21,6 +21,7 @@ def save_registration(cursor, username, password, image):
                     VALUES (%(username)s, %(password)s, %(image)s);
                    """,
                    {'username': username, 'password': password, 'image': image})
+
 
 @database_common.connection_handler
 def save_registration_without_image(cursor, username, password):
@@ -48,12 +49,12 @@ def get_users_password(cursor, username):
                     SELECT password FROM users
                     WHERE username = %(username)s;
                    """,
-                {'username': username })
+                   {'username': username})
     password = cursor.fetchone()
-    
+
     return password
 
-  
+
 @database_common.connection_handler
 def get_questions_by_user(cursor, user_id):
     cursor.execute("""
@@ -64,9 +65,9 @@ def get_questions_by_user(cursor, user_id):
                    """,
                    {'user_id': user_id})
     questions = cursor.fetchall()
-    
+
     return questions
- 
+
 
 @database_common.connection_handler
 def get_answers_by_user(cursor, user_id):
@@ -79,22 +80,39 @@ def get_answers_by_user(cursor, user_id):
                    """,
                    {'user_id': user_id})
     answers = cursor.fetchall()
-    
+
     return answers
 
 
 @database_common.connection_handler
-def get_comments_by_user(cursor, user_id):
+def get_question_comments_by_user(cursor, user_id):
     cursor.execute("""
-                    SELECT comment.id, comment.message AS comment, question.title, answer.message, question.id AS question_id
+                    SELECT comment.id, comment.message AS comment, question.title, question.id AS question_id
                     FROM comment
-                    FULL JOIN question ON (comment.question_id = question.id)
-                    FULL JOIN answer ON (comment.answer_id = answer.id)
+                    
+                    JOIN question ON (comment.question_id = question.id)
                     JOIN user_comment ON (comment_id = comment.id)
                     WHERE user_id = %(user_id)s; 
                    """,
                    {'user_id': user_id})
-    comments = cursor.fetchall()
-    
-    return comments
+    question_comments = cursor.fetchall()
 
+    return question_comments
+
+
+@database_common.connection_handler
+def get_answer_comments_by_user(cursor, user_id):
+    cursor.execute("""
+                    SELECT comment.id, comment.message AS comment,
+                    answer.message,
+                    question.id AS question_id, question.title AS title
+                    FROM comment
+                    FULL JOIN answer ON (comment.answer_id = answer.id)
+                    INNER JOIN question ON (answer.question_id = question.id)
+                    JOIN user_comment ON (comment_id = comment.id)
+                    WHERE user_id = %(user_id)s; 
+                   """,
+                   {'user_id': user_id})
+    answer_comments = cursor.fetchall()
+
+    return answer_comments
