bdf6c52a03d02d02eaa832bcc10c991fa7dccec4
Laszlo Jeszmas <rolne1992@gmail.com>
2018-01-25 12:06:21 +0100

    You cant comment, add question or answer if you are not logged in.

--- server.py
+++ server.py
@@ -13,8 +13,9 @@ def index():
 
 @app.route('/table')
 def table():
+    username = session['username'] if 'username' in session else 'Anonymus'
     info = data_handler.get_all_data('question')
-    return render_template("table.html", info=info)
+    return render_template("table.html", info=info, username=username)
 
 
 @app.route('/question/add_new_question', methods=['GET', 'POST'])
@@ -42,6 +43,7 @@ def question_details(question_id):
     view_number = info[0]['view_number']
     popular_number = info[0]['vote_number']
     tag = info_tag['name']
+    username = session['username'] if 'username' in session else 'Anonymus'
     answers = data_handler.get_answers_by_question_id(question_id)
     if request.method == 'GET':
         view_number += 1
@@ -50,7 +52,7 @@ def question_details(question_id):
     return render_template("question.html", question_id=question_id, question_data=question_data,
                            detail_data=detail_data,
                            picture_data=picture_data, view_number=view_number, popular_number=popular_number,
-                           tag=tag, answers=answers)
+                           tag=tag, answers=answers, username = username)
 
 
 @app.route('/like/<int:question_id>', methods=["GET"])
